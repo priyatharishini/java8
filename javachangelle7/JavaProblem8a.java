4bfafb2e3a7e4d267d0551093f0e5a510b2f649e
priyatharishini <61748229+priyatharishini@users.noreply.github.com>
2023-03-10 22:21:42 +0530

    Add files via upload

---
+++ javachangelle7/JavaProblem8a.java
@@ -0,0 +1,33 @@
+package javachangelle7;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JavaProblem8a {
+	public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+              
+            }
+            if (!set.add(nums[i])) {
+                return true;
+               
+            }
+            System.out.println(set); 
+        }
+        return false;
+	}
+	public static void main(String[] args) {
+		int[] nums = {1,0,1,1};
+        int k =1 ;
+        boolean result = containsNearbyDuplicate(nums, k);
+        System.out.println(result);	
+      
+	}
+
+}
---
+++ javachangelle7/JavaProblem8b.java
@@ -0,0 +1,36 @@
+package javachangelle7;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JavaProblem8b
+{
+public static boolean containsNearbyDuplicate(int[] nums, int k) {
+    if (nums == null || nums.length == 0 || k <= 0) {
+        return false;
+    }
+    Set<Integer> set = new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+        if (i > k) {
+            set.remove(nums[i - k - 1]);
+          
+        }
+        if (!set.add(nums[i])) {
+            return true;
+           
+        }
+       System.out.println(set); 
+ 
+    }
+    return false;
+}
+
+	public static void main(String[] args) 
+	{
+		int[] nums = {1,2,3,1,2,3};
+        int k =2 ;
+        boolean result = containsNearbyDuplicate(nums, k);
+        System.out.println(result);	
+	}
+
+}
